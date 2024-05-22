83ac8f077f7f8a43743343e2919f15d9ec630eda
KhyleSewpersaud <khylesewpersaud@gmail.com>
2024-05-22 13:24:02 -0400

    Did some unit tests on Check Button and url appending /#slide1

---
+++ front-end/src/__test__/App.test.jsx
@@ -0,0 +1,33 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import {describe, expect, it, vi} from 'vitest';
+import App from '../App.jsx'
+
+describe("Checking Check Button", () => {
+  it("Scrolls down to Questions component", () => {
+    render(<App />);
+
+    // Mock the scrollIntoView function
+    const scrollIntoViewMock = vi.fn();
+    Element.prototype.scrollIntoView = scrollIntoViewMock;
+    window.scrollTo = vi.fn()
+
+    const checkButton = screen.getByText("Check");
+
+    // Simulate clicking the "Check" button
+    fireEvent.click(checkButton);
+
+    // Ensure the mock function was called
+    expect(scrollIntoViewMock).toHaveBeenCalledWith({ behavior: 'smooth' });
+
+    // Optionally, you can check how many times it was called
+    expect(scrollIntoViewMock).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('Testing to see if URL hash redirect works', () => { 
+  it("Add's /#slide1 loads", async () => {
+    render(<App />);
+
+    expect(window.location.href).toEqual("http://localhost:3000/#slide1")
+  })
+ })
