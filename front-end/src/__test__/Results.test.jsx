dd743e9b3b684fbce668f62baf8dbe66036b8640
KhyleSewpersaud <khylesewpersaud@gmail.com>
2024-06-03 14:28:56 -0400

    fix2

--- front-end/src/__test__/Results.test.jsx
+++ front-end/src/__test__/Results.test.jsx
@@ -3,33 +3,33 @@ import {describe, expect, it, vi} from 'vitest';
 import Results from '../components/Results';
 
 
-// vi.mock('../components/BottleOutput', () => ({
-//     default: ({ image, quantity, className }) => (
-//       <div data-testid="bottle-output">
-//         <img src={image} alt="bottle" className={className} />
-//         <span>{quantity}</span>
-//       </div>
-//     ),
-//   }))
+vi.mock('../components/BottleOutput', () => ({
+    default: ({ image, quantity, className }) => (
+      <div data-testid="bottle-output">
+        <img src={image} alt="bottle" className={className} />
+        <span>{quantity}</span>
+      </div>
+    ),
+  }))
 
   
-// describe('Results Component', () => {
-//     it('should generate a solution', () => {
-//         const mockProps = {
-//         hours: "08",
-//         minute: "30",
-//         period: "AM",
-//         gender: "male",
-//         weight: "70",
-//         climate: "neutral",
-//         exerciseMinutes: 60,
-//         bottles: [0.5, 0.5, ...Array(16).fill(0)], // 1 of each bottle type
-//         weightUnit: "kg",
-//         directInputUnit: "ml",
-//         directInput: 500,
-//         };
+describe('Results Component', () => {
+    // it('should generate a solution', () => {
+    //     const mockProps = {
+    //     hours: "08",
+    //     minute: "30",
+    //     period: "AM",
+    //     gender: "male",
+    //     weight: "70",
+    //     climate: "neutral",
+    //     exerciseMinutes: 60,
+    //     bottles: [0.5, 0.5, ...Array(16).fill(0)], // 1 of each bottle type
+    //     weightUnit: "kg",
+    //     directInputUnit: "ml",
+    //     directInput: 500,
+    //     };
 
-//         render(<Results {...mockProps} />);
+    //     render(<Results {...mockProps} />);
 
-//     });
-// });
+    // });
+});
