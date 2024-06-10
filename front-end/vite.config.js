4a6db0e53dff2a4b46784c975fcf9cb97c5f8adf
KhyleSewpersaud <khylesewpersaud@gmail.com>
2024-06-10 13:33:52 -0400

    Installed testing files

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/__test__/setup.jsx'
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/__test__/setup.jsx",
+    coverage: {
+      provider: "v8",
+    },
+  },
+});
